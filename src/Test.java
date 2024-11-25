f4f83fa62854e57bff0cc236bfc0b44a178602ea
yunahamazawa <yunahamazawa821@gmail.com>
2024-11-25 22:03:32 +0900

    演習⑤

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,20 @@ public class Test {
 		saburo.age = 0;
 		System.out.println(saburo.name);
 		System.out.println(saburo.age);
-		}
+
+	
+		Person namaenashi= new Person();
+		namaenashi.name = "名前なし";
+		namaenashi.age = 25;
+		System.out.println(namaenashi.name);
+		System.out.println(namaenashi.age);
+
+
+		Person hanako= new Person();
+		hanako.name = "hanako";
+		hanako.age = 17;
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
+	}
 }
